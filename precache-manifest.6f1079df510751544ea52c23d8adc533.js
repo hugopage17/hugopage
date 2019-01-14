8fe877b316f4063c4427b429cd7c8cbadeb8c604
hugopage17 <hugopage0017@gmail.com>
2019-01-15 10:56:26 +1300

    Updates

---
+++ precache-manifest.6f1079df510751544ea52c23d8adc533.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "edad9bede008998d58783fec4b05ccc2",
+    "url": "/hugopage/static/media/python_logo.edad9bed.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hugopage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9cf4a14cb5024b5d6e988c1519570ce3",
+    "url": "/hugopage/static/media/html.9cf4a14c.png"
+  },
+  {
+    "revision": "34b9114596be375ae7ed54d5e96c4a08",
+    "url": "/hugopage/static/media/React-Logo.34b91145.png"
+  },
+  {
+    "revision": "349f55972a6973626ee5",
+    "url": "/hugopage/static/js/runtime~main.349f5597.js"
+  },
+  {
+    "revision": "45bd787e346d42e8ff51",
+    "url": "/hugopage/static/js/main.45bd787e.chunk.js"
+  },
+  {
+    "revision": "fcea78fc72ace6696fa6",
+    "url": "/hugopage/static/js/1.fcea78fc.chunk.js"
+  },
+  {
+    "revision": "45bd787e346d42e8ff51",
+    "url": "/hugopage/static/css/main.31c29e00.chunk.css"
+  },
+  {
+    "revision": "aecd887218ef74bee23ec6ce071036f2",
+    "url": "/hugopage/index.html"
+  }
+];
